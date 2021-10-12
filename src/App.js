13b096555701b5a5971738560f17e77c2b71034b
Anna <anna.mcphee@gmail.com>
2021-10-12 14:59:06 -0500

    add basic api fetch

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@
       results: []
     }
 
+    // this will need to fetch GET /sites/<site_id>/jetpack-search-stats on WPCOM V2 once auth is configured
+
+    componentDidMount() {
+      fetch('')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ results: data })
+      })
+      .catch(console.log)
+    }
+
     render () {
       return (
         // JSX to render goes here...
