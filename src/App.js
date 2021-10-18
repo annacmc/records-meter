cfcd0b0517d81e62b6e4dc23fbe3e363b790277e
Anna <anna.mcphee@gmail.com>
2021-10-18 16:22:31 -0500

    add charts.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,32 @@
-  // src/App.js
-
-  import React, {Component} from 'react';
-
-  class App extends Component {
-
-    constructor(props){
+import React from 'react';
+import './App.css';
+  
+class App extends React.Component {
+  
+  constructor(props){
       super(props)
       this.state = {
-        interests: []
+        response: []
       }
     }
- 
+  
   async componentDidMount() {
     const response = await fetch( 'https://public-api.wordpress.com/wpcom/v2/read/interests' );
     const json = await response.json();
     this.setState( { response: json } );
     console.log( json );
   }
- 
+  
   render() {
     return (
         <div className="App">
           <h1>Reader interests</h1>
           <ul>
-            { this.state.response?.interests.map( interest => ( <li key={interest.slug}>{interest.title}</li>) ) }
+            { this.state.response.interests?.map( interest => ( <li key={interest.slug}>{interest.title}</li>) ) }
           </ul>
         </div>
     );
   }
 }
-
-  export default App;
+  
+export default App;
