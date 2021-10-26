2167f52b90a5b262906ff8b2fc24afca69e1b7e4
Anna <anna.mcphee@gmail.com>
2021-10-26 12:09:32 -0500

    remove unwanted charting items/labels/lines

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import { Chart, registerables } from 'chart.js';
+import React from "react";
+import { Chart, registerables } from "chart.js";
 
 Chart.register(...registerables);
 
 export class BarChart extends React.Component {
-    constructor(props) {
-      super(props);
-      this.canvasRef = React.createRef();
-      this.state = {
-      labels : this.props.data.map(d => d.data.label),
-      colors : this.props.data.map(d => d.data.backgroundColor),
-      moredata : this.props.data.map(d => d.data)
-      }
-    }
-  
-    componentDidMount() {    
-
-      this.myChart = new Chart(this.canvasRef.current, {
-        type: 'bar',
-        options: {
-          indexAxis: 'y',
-            maintainAspectRatio: false,
-            scales: {
-              x: {
-                stacked: true,
-              },
-              y: {
-                stacked: true
-              }
-            }
-        },
-
-        data:{
-          labels: ["Anna's Chart"],
-          datasets: this.props.data.map(d => d.data)
-        }        
-
-      });
-    }
-  
-    render() {
-      return (
-          <canvas ref={this.canvasRef} />
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.canvasRef = React.createRef();
+    this.state = {
+      labels: this.props.data.map((d) => d.data.label),
+      colors: this.props.data.map((d) => d.data.backgroundColor),
+      moredata: this.props.data.map((d) => d.data),
+    };
   }
-  
-  
+
+  componentDidMount() {
+    this.myChart = new Chart(this.canvasRef.current, {
+      type: "bar",
+      options: {
+        indexAxis: "y",
+        legend: { display: false },
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            stacked: true,
+            grid: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              display: false,
+            },
+          },
+          y: {
+            stacked: true,
+            grid: {
+              display: false,
+              drawBorder: false,
+            },
+            ticks: {
+              display: false,
+            },
+          },
+        },
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: false,
+          },
+        },
+      },
+      data: {
+        labels: ["Anna's Chart"],
+        datasets: this.props.data.map((d) => d.data),
+      },
+    });
+  }
+
+  render() {
+    return <canvas ref={this.canvasRef} />;
+  }
+}
