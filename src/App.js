ccb1497fb7c9aa576699106f09706656d1f5ab79
Anna <anna.mcphee@gmail.com>
2021-10-26 13:03:56 -0500

    create basic login and auth components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import './App.css';
-import getFeeds from './components/getFeeds.js';
-import { BarChart } from './components/BarChart';
-  
+import React from "react";
+import "./App.css";
+import getFeeds from "./components/getFeeds.js";
+import { BarChart } from "./components/BarChart";
+
 class App extends React.Component {
-  
-  constructor(props){
-      super(props)
-      this.state = {
-        data: getFeeds(),
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: getFeeds(),
+    };
+  }
 
   render() {
     return (
-      <div>  
-<BarChart
-            data={this.state.data}
-          />
-          
-    </div>
+      <div>
+        <BarChart data={this.state.data} />
+      </div>
     );
-    }
+  }
 }
 export default App;
