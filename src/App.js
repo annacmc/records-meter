5a93fd2e1d75d01ccf711889780cbff9a305cdc4
Anna <anna.mcphee@gmail.com>
2021-10-27 13:47:28 -0500

    formatting update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import './App.css';
-import getFeeds from './components/getFeeds.js';
-import { BarChart } from './components/BarChart';
-  
+import React from "react";
+import "./App.css";
+import getFeeds from "./components/getFeeds.js";
+import { BarChart } from "./components/BarChart";
+
 class App extends React.Component {
-  
-  constructor(props){
-      super(props)
-      this.state = {
-        data: getFeeds(),
-      }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: getFeeds(),
+    };
+  }
 
   render() {
     return (
-      <div style={{display: 'flex',  justifyContent:'center', alignItems:'center', width:'50%'}}>
-<BarChart
-        data={this.state.data}
-          />
-          
-    </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "50%",
+        }}
+      >
+        <BarChart data={this.state.data} />
+      </div>
     );
-    }
+  }
 }
 export default App;
--- src/components/getFeeds.js
+++ src/components/getFeeds.js
@@ -1,30 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function getFeeds() {
   let feeds = [];
   let numItems = 5;
   let totalCount = 1000;
   let currentCount = 0;
-  let colors = [
-    "#3895BA",
-    "#E68B28",
-    "#AF7DD1",
-    "#00BA37",
-    "#DEB100",
-  ];
+  let colors = ["#3895BA", "#E68B28", "#AF7DD1", "#00BA37", "#DEB100"];
 
   for (var i = 0; i < numItems; i++) {
+    let mydata = getRandomArray(colors[i]);
 
-    let mydata = getRandomArray(colors[i])
-    
     feeds.push({
       data: mydata,
     });
-    currentCount = currentCount + mydata.data[0]
+    currentCount = currentCount + mydata.data[0];
   }
 
   feeds.push({
-    data: getRemainingSpace(totalCount,currentCount),
+    data: getRemainingSpace(totalCount, currentCount),
   });
 
   return feeds;
@@ -44,11 +37,11 @@ function getRandomArray(color) {
 }
 
 // Generation of empty space (to work with ChartJS styling)
-function getRemainingSpace(total,current) {
-  let data = total - current
+function getRemainingSpace(total, current) {
+  let data = total - current;
   return {
     data: [data],
-    label: "Remaining"+ " (" + data + ")",
+    label: "Remaining" + " (" + data + ")",
     backgroundColor: "rgb(245,245,245)",
   };
 }
