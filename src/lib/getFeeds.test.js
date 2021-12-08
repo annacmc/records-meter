f4c7a629faf2b8b4fd9fc713174727c80432ac6c
Anna <anna.mcphee@gmail.com>
2021-12-08 14:46:52 -0700

    export & test getRemainingSpace method

--- src/lib/getFeeds.test.js
+++ src/lib/getFeeds.test.js
@@ -1,4 +1,4 @@
-const getFeeds = require("./getFeeds");
+import * as getFeeds from "./getFeeds";
 
 const testData = {
   last_indexed_date: "2021-07-06T19:35:18+00:00",
@@ -13,4 +13,14 @@ const testData = {
 test("compares post_count to post_type_breakdown summed", () => {
   const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
   expect(testData.post_count).toEqual(sumValues(testData.post_type_breakdown));
+});
+
+test("tests getRemainingSpace", () => {
+  const remainingSpace = getFeeds.getRemainingSpace(20, 5);
+
+  expect(remainingSpace).toEqual({
+    data: [15],
+    label: "Remaining",
+    backgroundColor: "rgb(245,245,245)",
+  });
 });
