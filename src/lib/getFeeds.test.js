36ff971732a07162b8ce4bfb462d8a360b7c220f
Anna <anna.mcphee@gmail.com>
2021-12-08 12:33:22 -0700

    add a basic test

--- src/lib/getFeeds.test.js
+++ src/lib/getFeeds.test.js
@@ -1,5 +1,16 @@
-const getFeeds = require('./getFeeds');
+const getFeeds = require("./getFeeds");
 
-test('adds 1 + 2 to equal 3', () => {
-  expect((1 + 2)).toBe(3);
+const testData = {
+  last_indexed_date: "2021-07-06T19:35:18+00:00",
+  post_count: 136,
+  post_type_breakdown: {
+    post: 104,
+    page: 17,
+    attachment: 15,
+  },
+};
+
+test("compares post_count to post_type_breakdown summed", () => {
+  const sumValues = (obj) => Object.values(obj).reduce((a, b) => a + b);
+  expect(testData.post_count).toEqual(sumValues(testData.post_type_breakdown));
 });
