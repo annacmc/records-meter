3513350b91de81ce218b77650d84c071ceb03ebc
Anna <anna.mcphee@gmail.com>
2022-01-11 19:08:37 -0600

    remove superfluous commenting

--- src/components/NoticeBox.js
+++ src/components/NoticeBox.js
@@ -1,39 +1,13 @@
-
-
-// Handle users who are currently over their record limitIf a user has paid for a 1000 record tier, 
-// and they currently have 1200 records, then:
-
-// the chart should not melt/explode and still display the correct breakdown
-// we should show the user a nudge that they'll be upgraded for their next billing cycle
-
-
-// Handle users who don't have any records at all 
-
-// For users who don't have any records at all (but indexing is completed), 
-// we could either show an empty bar or a custom message.
-
-// In any case, let's make sure the chart still renders correctly when no data is provided.
-
-// Handle a site that hasn't been indexed yet
-
-// If there's no indexing date and no records, the site probably hasn't been indexed yet 
-// and we should show different messaging to the user indicating that they should wait for indexing to complete.
-
 import React from "react";
 
 export function NoticeBox(props) {
-
-  if ( props.errors.length == 0 ) {
+  if (props.errors.length == 0) {
     return null;
   }
 
   return (
-    <div className = {props.className}> 
-      {props.errors && (    
-        <p>
-          {props.errors[0]}
-        </p>
-      )}
+    <div className={props.className}>
+      {props.errors && <p>{props.errors[0]}</p>}
     </div>
   );
 }
