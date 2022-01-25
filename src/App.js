896758022eebf69b95e2126d46341a461b37fbd6
Anna <anna.mcphee@gmail.com>
2022-01-25 15:17:14 -0600

    remove redundant console logging

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ class App extends React.Component {
     this.state = {
       recordInfo: getRecordInfo(getData().data, getData().planInfo),
     };
-    console.log(this.state.recordInfo.data);
   }
 
   render() {
